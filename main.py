ffe2ffe708ada600bc50e8705832615c1c37d48d
big215 <glenn.j.gilroy@gmail.com>
2024-09-19 22:19:50 -0400

    initial refactor

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 def main():
 	path_to_file = "books/frankenstein.txt"
 
+	book = open_book(path_to_file)
+
+	print(word_count(book))
+
+def open_book(path_to_file):
 	with open(path_to_file) as f:
 		file_contents = f.read()
+	return file_contents
 
-	words = file_contents.split()
-
-	print(len(words))
+def word_count(book_text):
+	words = book_text.split()
+	return len(words)
 
 main()
